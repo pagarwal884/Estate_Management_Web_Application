ea28cede9e22b6ab90e469dadbb4ab49c633a5f2
pagarwal884 <pagarwal1145@gmail.com>
2026-05-20 00:24:13 +0530

    Added the logic of profile picture in this project

--- Backend/controller/user.controller.js
+++ Backend/controller/user.controller.js
@@ -1,15 +1,20 @@
 import User from "../models/usermodels.js"
+import { UploadToCloudinary } from "../utils/uploadTocloudinary.js"
 
-// get profile
-export const getProfile = async(req, res)=> {
+// Get Profile
+export const getProfile = async (req, res) => {
     try {
-        const user = await User.findById(req.user._id).select("-password")
+
+        const user = await User.findById(req.user._id)
+            .select("-password")
+
         res.status(200).json({
             success: true,
             user
         })
 
     } catch (err) {
+
         res.status(500).json({
             success: false,
             message: err.message
@@ -18,13 +23,15 @@ export const getProfile = async(req, res)=> {
 }
 
 
-// get Public ID
+// Get Public Profile
+export const getPublicprofile = async (req, res) => {
 
-export const getPublicprofile = async(req, res) => {
     try {
-        const user = await User.findById(req.params.id).select("name profilePic role createdAt")
 
-        if(!user) {
+        const user = await User.findById(req.params.id)
+            .select("name profilePic role createdAt")
+
+        if (!user) {
             return res.status(404).json({
                 success: false,
                 message: "User not found"
@@ -35,7 +42,9 @@ export const getPublicprofile = async(req, res) => {
             success: true,
             user
         })
+
     } catch (err) {
+
         res.status(500).json({
             success: false,
             message: err.message
@@ -43,21 +52,55 @@ export const getPublicprofile = async(req, res) => {
     }
 }
 
-// Update a Profile
-export const updateProdile = async (req, res) => {
+
+// Update Profile
+export const updateProfile = async (req, res) => {
+
     try {
-        const {name, phone, address, removeProfilePic} = req.body
+
+        const { name, phone, address, removeProfilePic } = req.body
+
         const user = await User.findById(req.user._id)
 
-        if(!user){
-            return res.status(404),json({
+        if (!user) {
+            return res.status(404).json({
                 success: false,
                 message: "User not found"
             })
         }
 
-        // image handing
-    } catch (err) {
+        // Upload image
+        if (req.file) {
+
+            const result = await UploadToCloudinary(
+                req.file.buffer,
+                "profiles"
+            )
+
+            // Save image URL
+            user.profilePic = result.secure_url
+        }else if (removeProfilePic === "true") {
+            user.profilePic = ""
+        }
+
+        // Update fields
+        if (name) user.name = name
+        if (phone) user.phone = phone
+        if (address) user.address = address
         
+        await user.save()
+
+        res.status(200).json({
+            success: true,
+            message: "Profile updated successfully",
+            user
+        })
+
+    } catch (err) {
+
+        res.status(500).json({
+            success: false,
+            message: err.message
+        })
     }
 }
