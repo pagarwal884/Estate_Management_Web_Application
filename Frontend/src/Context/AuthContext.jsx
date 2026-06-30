dedf9a34bf0050b89c867de917672a15f5574ddc
pagarwal884 <pagarwal1145@gmail.com>
2026-07-01 01:13:52 +0530

    Started to work on AuthContetx file and Authentication system which i will complete that next day

--- Frontend/src/Context/AuthContext.jsx
+++ Frontend/src/Context/AuthContext.jsx
@@ -1,11 +1,41 @@
-import React from 'react'
+import React, { createContext, useContext, useEffect, useState } from 'react'
+import axios from 'axios'
+import API_URL from '../config.js'
+import { useNavigate } from 'react-router-dom'
 
-const AuthContext = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+const AuthContext = createContext()
+
+export const AuthProvider = ({ children }) => {
+    const [user, setUser] = useState(null)
+
+    const [token, setToken] = useState(
+        localStorage.getItem("token") ||
+        sessionStorage.getItem("token") ||
+        null
+    )
+
+    const [loading, setLoading] = useState(true)
+    const navigate = useNavigate()
+
+    useEffect(() => {
+        if (token) {
+            const storedUser =
+                localStorage.getItem("user") ||
+                sessionStorage.getItem("user")
+
+            if (storedUser) {
+                setUser(JSON.parse(storedUser))
+            }
+        }
+
+        setLoading(false)
+    }, [token])
+
+    return (
+        <AuthContext.Provider value={{ user, setUser, token, setToken, loading }}>
+            {children}
+        </AuthContext.Provider>
+    )
 }
 
-export default AuthContext
+export const useAuth = () => useContext(AuthContext)
